f92c3b99f71a7fa1c10c63423730d81a0b317d30
Miguel Perez <miguel@thecroux.co>
2015-10-26 12:39:43 -0400

    Add gulp serve task

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,49 @@
 'use strict';
 
 var gulp = require('gulp'),
-    plugins = require('gulp-load-plugins')();
+    fs = require('fs'),
+    path = require('path'),
+    plugins = require('gulp-load-plugins')(),
+    getFolders = function(dir){
+      return  fs.readdirSync(dir)
+        .filter(function(file) {
+          return fs.statSync(path.join(dir, file)).isDirectory();
+        });
+    };
 
-var scripts = [
+var
+  scripts = [
     './src/**/*.js',
+  ],
+  demos = [
+    './demos/barebones',
+    './demos/csstricks',
   ],
   tests = [
     './tests/**/*.js'
   ],
-  allScripts = scripts.concat(tests);
+  allScripts = scripts.concat(tests),
+  demoScripts = scripts.concat('./node_modules/jquery/dist/jquery.js');
+
+/** Serve demos for testing */
+gulp.task('serve', ['copyDemoFiles'], function() {
+  gulp.src('demos')
+    .pipe(plugins.webserver({
+      open: true
+    }));
+});
 
 /** Run all unit tests */
 gulp.task('test', function() {
   return gulp.src('./tests/index.html')
         .pipe(plugins.qunit());
+});
+
+/** Run all unit tests */
+gulp.task('copyDemoFiles', function() {
+  return gulp.src(demoScripts)
+        .pipe(gulp.dest(demos[0]))
+        .pipe(gulp.dest(demos[1]));
 });
 
 /** Lint JavaScript */
@@ -36,7 +65,7 @@ gulp.task('uglify', function () {
 
 /** Watch changes */
 gulp.task('watch', function() {
-  gulp.watch(allScripts, ['jshint', 'test']);
+  gulp.watch(allScripts, ['jshint', 'test', 'copyDemoFiles']);
 });
 
 /** Default task */
